7722b708ab1681ab52cde5e0bfd841dc7875e663
Marknjo <marknjo7@gmail.com>
2022-10-12 18:14:41 +0300

    ci(lab): create a multipages template, tailwind and normal sass settings

---
+++ multipages-template/src/pages/template/js/template.ts
@@ -0,0 +1,8 @@
+import '~/js/fouc-fix';
+
+import '@/template/styles/template-tailwind.scss';
+import '@/template/styles/base.scss';
+
+console.log('Template Page');
+
+export {};
---
+++ multipages-template/vite.config.ts
@@ -0,0 +1,25 @@
+// vite.config.js
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+const root = resolve(__dirname, 'src');
+
+console.log(resolve(root, 'pages', 'about', 'about.html'));
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~': root,
+      '@': resolve(root, 'pages'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        template: resolve(root, 'pages', 'template', 'template.html'),
+        // about: resolve(root, 'pages', 'rich-text-editor', 'rte.html'),
+      },
+    },
+  },
+});
